8a60998540dd1f2b2c2dfbc57b270c3379a86f14
Akachukwu <udechukwuakachukwu@gmail.com>
2024-05-09 23:38:02 +0100

    created all the major  routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,37 @@
 import asyncHandler from 'express-async-handler'
 // @desc fot the user authentication
-// route POST => /api/users
+// route POST => /api/users/auth
 // access public
 const userAuth = asyncHandler( async (req, res) => {
-    res.status(200);
-    throw new Error("Something went wrong");
     res.status(200).json({message: 'User Authentication'})
 })
 
-export {userAuth}
+// @desc fot the user registration
+// route POST => /api/users
+// access public
+const userRegistration = asyncHandler( async (req, res) => {
+    res.status(200).json({message: 'User Registration'})
+})
+
+// @desc fot the user Logout
+// route POST => /api/users/logout
+// access public
+const logoutUser = asyncHandler( async (req, res) => {
+    res.status(200).json({message: 'User Logout'})
+})
+
+// @desc fot the user profile
+// route GET => /api/users/profile
+// access private routes
+const getUserProfile = asyncHandler( async (req, res) => {
+    res.status(200).json({message: 'Get the user profile'})
+})
+
+// @desc fot the user Logout
+// route PUT => /api/users/profile
+// access private
+const updateUserProfile = asyncHandler( async (req, res) => {
+    res.status(200).json({message: 'User Update Profile'})
+})
+
+export {userAuth , userRegistration, getUserProfile, updateUserProfile, logoutUser}
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import { userAuth } from '../controllers/userController.js';
+import { userAuth , userRegistration, getUserProfile, updateUserProfile, logoutUser } from '../controllers/userController.js';
 
 const router = express.Router();
 
-router.post('/auth', userAuth)
+router.post('/auth', userAuth);
+router.post('/', userRegistration);
+router.post('/logout', logoutUser);
+router.route('/profile').get(getUserProfile).put(updateUserProfile)
 
 export default router

8f58077f95687992067c4e4b292ecc66ba81b8b3
Akachukwu <udechukwuakachukwu@gmail.com>
2024-05-10 08:46:34 +0100

    Connected the mongodb to the app

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import userRoute from './routes/userRoute.js'
 import { errorHandler, notFound } from './middlewares/errorMiddleware.js';
+import connectDB from './configs/dbConn.js';
+
+
 const app = express();
+dotenv.config();
+
 const PORT = process.env.PORT || 8000;
 
+// dbConnection
+connectDB()
 
-// middleware
-
+//route middleware
 app.use("/api/users", userRoute)
 
 
@@ -15,9 +22,11 @@ app.get('/', (req, res) => {
     res.status(200).json({message: 'Ultra Auth root page'})
 })
 
+// error handling middleware
 app.use(notFound)
 app.use(errorHandler)
 
+// app listing on port
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
 });
